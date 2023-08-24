1349b47bf97731f11bdeaeafc6a2db212f5b0ed2
techmoves <muhndlovu28@gmail.com>
2023-08-24 21:57:50 +0200

    change  to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'pages#hello'
+  # get 'pages#hello'
   root 'pages#hello'
   # Defines the root path route ("/")
   # root "articles#index"
